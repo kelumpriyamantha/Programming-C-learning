a70a18897569cac5d13129f7bffc16bdd0ad5bfd
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2020-10-13 23:43:11 +0530

    C 4

---
+++ C 4/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1,n2,total;
+    printf("Enter the first number");
+    scanf("%d", &n1);
+    printf("Enter the second number");
+    scanf("%d", &n2);
+    total=(n1+n2);
+    printf("Total is %d\n",total);
+
+}
