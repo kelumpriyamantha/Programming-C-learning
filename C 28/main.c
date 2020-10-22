46d3adf33f229685b2b8260ab1d8bb074fa631d3
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2020-10-22 20:18:25 +0530

    C 29

--- C 28/main.c
+++ C 28/main.c
@@ -3,15 +3,5 @@
 
 int main()
 {
-     int n, i, sum = 0;
-    printf("Enter a positive integer: ");
-    scanf("%d", &n);
-    i = 1;
 
-    while (i <= n) {
-        sum += i;
-        ++i;
-    }
-
-    printf("Sum = %d", sum);
 }
