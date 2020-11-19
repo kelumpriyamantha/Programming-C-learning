93dc81373da9d0069b129774fc363d65f6bd0c5d
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2020-11-20 00:22:15 +0530

    C 64

---
+++ C 64/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+   float no1,no2,total,avg;
+   printf("Enter the first number");
+   scanf("%f",&no1);
+   printf("Enter the second number");
+   scanf("%f",&no2);
+   total=(no1+no2);
+   avg=(no1+no2)/2;
+   printf("Total is %f\n",total);
+   printf("Average is %f\n",avg);
+    return 0;
+}
