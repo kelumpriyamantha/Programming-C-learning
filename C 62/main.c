30eb63b4d09f2116a143f79ed04a84eef5c356d5
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2020-11-19 09:47:40 +0530

    C 62

---
+++ C 62/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findSum()
+{
+    int a=15,b=60,c;
+    c=a+b;
+ return c;
+}
+
+int main()
+{
+    int ans=findSum();
+    printf("The sum is %d \n",ans);
+}
