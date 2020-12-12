c1fc30b8997939d65dca8ff039ed3fe8e0589e9c
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2020-12-12 14:27:27 +0530

    2

--- Practical 01/main.c
+++ Practical 01/main.c
@@ -1,49 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+double findProduct(int a,float b)
+{
+    double product;
+    product=(double)a*(double)b;
+return product;
+}
 int main()
 {
-
-   int n,i, scaler_sum1=0, scaler_product_sum=0, scaler_sum2=0;
-
-   printf("enter the size for array");
-   scanf("%d",&n);
-
-   int arr1[n],arr2[n],vector_sum[n],vector_product[n],scaler_product[n];
-
-   for(i=0;i<n;i++)
-   {
-       printf("enter a value for array 1 :");
-       scanf("%d",&arr1[i]);
-   }
-   for(i=0;i<n;i++)
-   {
-       printf("enter a value for array 2 :");
-       scanf("%d",&arr2[i]);
-   }
-   for(i=0;i<n;i++)
-   {
-       scaler_sum1=scaler_sum1*arr1[i];
-       scaler_sum2=scaler_sum2*arr2[i];
-   }
-   printf("\nArray 1 scaler sum: %d\n",scaler_sum1);
-   printf("\nArray 2 scaler sum: %d\n",scaler_sum2);
-   printf("\nvector_sum: ");
-   for(i=0;i<n;i++)
-   {
-       vector_sum[i]=arr1[i]+arr2[i];
-       printf("%d",vector_sum[i]);
-   }
-   printf("\n\nvector_product:");
-   for(i=0;1<n;i++);
-   {
-       vector_product[i]=arr1[i]*arr2[i];
-       printf("%d",vector_product[i]);
-   }
-   printf("\n");
-   for(i=0;i<n;i++)
-   {
-       scaler_product[i]=arr1[i]*arr2[i];
-       scaler_product_sum=scaler_product_sum+scaler_product[i];
-   }
-   printf("scaler_product: %d\n",scaler_product_sum);
+    int x;
+    float y;
+    printf("Enter the two numbers");
+    scanf("%d %f",&x,&y);
+    printf("\nProduct is %1f\n",findProduct(x,y));
 }
+
+
