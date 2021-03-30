cab1314a846d37718443f4f3d416791d3d0be373
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2021-03-30 10:55:05 +0530

    jhhhhh

---
+++ cexam/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+float arr[4][4];
+float val;
+
+for (int i =0; i<4; i++){
+for (int j = 0; i<4; j++){
+
+    printf("Enter number : ");
+    scanf("%f",&arr[i,j]);
+}
+
+}
